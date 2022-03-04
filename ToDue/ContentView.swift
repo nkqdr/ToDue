2e33f534d196f1c06ddba4ee9c9d85beeaf90e01
Niklas Kuder <nkqdr@users.noreply.github.com>
2022-03-04 21:43:35 +0100

    Initial Commit

---
+++ ToDue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDue
+//
+//  Created by Niklas Kuder on 04.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDue/ToDueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDueApp.swift
+//  ToDue
+//
+//  Created by Niklas Kuder on 04.03.22.
+//
+
+import SwiftUI
+
+@main
+struct ToDueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
