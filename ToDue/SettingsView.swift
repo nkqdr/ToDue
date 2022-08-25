e709613a459f4224fdff013735de93feae2aac3c
Niklas Kuder <nkqdr@users.noreply.github.com>
2022-08-25 10:54:16 +0200

    Pretty much finalized the new DetailView and also slightly improved the settings page

--- ToDue/SettingsView.swift
+++ ToDue/SettingsView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.openURL) var openURL
     @State private var toggle = false
-//    @Binding var isPresented: Bool
     enum AppTheme: String, CaseIterable, Identifiable {
         case system, light, dark
         var id: Self { self }
@@ -19,58 +19,79 @@ struct SettingsView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    Group {
-                        Section("General") {
-                            NavigationLink("Something") {
-                                
-                            }
-                            NavigationLink("Something else") {
-                                
-                            }
-                        }
-                        
-                        Section("Appearance") {
-                            Menu {
-                                Picker(selection: $selectedFlavor, label: Text("Select Theme")) {
-                                    ForEach(AppTheme.allCases) { appTheme in
-                                        Text(appTheme.rawValue.capitalized).tag(appTheme)
-                                    }
-                                }
-                            } label: {
-                                HStack {
-                                    Text("App Theme")
-                                        .foregroundColor(Color("Text"))
-                                    Spacer()
-                                    Text(selectedFlavor.rawValue.capitalized)
-                                        .foregroundColor(.secondary)
-                                }
-                            }
-                        }
-                        Section("Help") {
-                            NavigationLink("Some help") {
-                                
-                            }
-                        }
-                    }
-                    .listRowBackground(Color("Accent2").opacity(0.3))
-                    Group {
-                        Divider()
-                        Text("Made in Karlsruhe with ♡")
-                            .foregroundColor(.secondary)
-                            .font(.footnote)
-                    }
-                    .listRowSeparator(.hidden)
-                    .listRowInsets(.none)
-                    .listRowBackground(Color("Accent2").opacity(0))
+            List {
+                Group {
+                    generalSettings
+                    appearanceSettings
+                    helpSettings
+                    settingsFooter
                 }
-                .background(Color("Background"))
+                .listRowBackground(Color("Accent2").opacity(0.3))
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("Background"))
             .navigationTitle("Settings")
         }
         .navigationViewStyle(.stack)
+    }
+    
+    var generalSettings: some View {
+        Section("General") {
+            NavigationLink("Something") {
+                
+            }
+            NavigationLink("Something else") {
+                
+            }
+        }
+    }
+    
+    var appearanceSettings: some View {
+        Section("Appearance") {
+            Menu {
+                Picker(selection: $selectedFlavor, label: Text("Select Theme")) {
+                    ForEach(AppTheme.allCases) { appTheme in
+                        Text(appTheme.rawValue.capitalized).tag(appTheme)
+                    }
+                }
+            } label: {
+                HStack {
+                    Text("App Theme")
+                        .foregroundColor(Color("Text"))
+                    Spacer()
+                    Text(selectedFlavor.rawValue.capitalized)
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+    }
+    
+    var helpSettings: some View {
+        Section("Help") {
+            NavigationLink(destination: EmptyView()) {
+                Label("Some help", systemImage: "questionmark.circle.fill")
+                    .foregroundColor(Color("Text"))
+            }
+            Button {
+                let email = ContactEmail(toAddress: "contact@niklas-kuder.de", subject: "App contact inquiry", messageHeader: "Please enter your message below")
+                email.send(openURL: openURL)
+            } label: {
+                Label("Contact", systemImage: "person.fill.questionmark")
+            }
+        }
+    }
+    
+    var settingsFooter: some View {
+        Section {
+            HStack {
+                Image(systemName: "heart")
+                VStack(alignment: .leading) {
+                    Text("Made with Love in Karlsruhe")
+                    Text("Version 0.1.0")
+                }
+                .font(.footnote)
+            }
+            .foregroundColor(.secondary)
+        }
     }
     
     func placeOrder() { }
