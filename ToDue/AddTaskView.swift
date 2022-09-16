6b3d6b8dd094ed2b5858bafccc415a7ee30a0a20
Niklas Kuder <nkqdr@users.noreply.github.com>
2022-09-16 07:58:06 +0200

    Some minor tweaks to the new feature

--- ToDue/AddTaskView.swift
+++ ToDue/AddTaskView.swift
@@ -25,14 +25,16 @@ struct AddTaskView: View {
         return NavigationView {
             Form {
                 Group {
+                    Section {
+                        TextField("Title", text: $taskEditor.taskTitle)
+                            .onChange(of: taskEditor.taskTitle, perform: taskEditor.changeTitle)
+                    }
                     Section("Information") {
                         Picker("Task type", selection: $taskEditor.hasDeadline) {
                             Text("Deadline").tag(true)
                             Text("No Deadline").tag(false)
                         }
                         .pickerStyle(.segmented)
-                        TextField("Title", text: $taskEditor.taskTitle)
-                            .onChange(of: taskEditor.taskTitle, perform: taskEditor.changeTitle)
                         if taskEditor.hasDeadline {
                             DatePicker("Due date:", selection: $taskEditor.taskDueDate, in: dateRange, displayedComponents: .date)
                         }
@@ -44,6 +46,10 @@ struct AddTaskView: View {
                         }
                     }
                     Section("Additional Notes: (Optional)") {
+///                       Possibly replace this in the future?
+//                        TextField("Test", text: $taskEditor.taskDescription,  axis: .vertical)
+//                            .lineLimit(5...10)
+//                            .disableAutocorrection(true)
                         TextEditor(text: $taskEditor.taskDescription)
                             .frame(minHeight: 100)
                             .disableAutocorrection(true)
@@ -53,6 +59,7 @@ struct AddTaskView: View {
                 .themedListRowBackground()
             }
             .navigationTitle(editMode ? "Edit task" : "New task")
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button("Cancel") {
