a30d4c6a585252f6a4662c01082d68c94b09ec0d
Niklas Kuder <nkqdr@users.noreply.github.com>
2023-07-11 09:19:30 +0200

    Slightly refactored date extension

--- ToDue/Extensions/DateExtension.swift
+++ ToDue/Extensions/DateExtension.swift
@@ -33,20 +33,17 @@ extension DateComponents {
 
 extension Date {
     public var removeTimeStamp : Date? {
+        // Maybe a better solution? To be tested...
+        // return Calendar.current.startOfDay(for: self)
+        
        guard let date = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month, .day], from: self)) else {
         return nil
        }
        return date
     }
     
-    private func isSameXAs(_ otherDate: Date, comps: Set<Calendar.Component>) -> Bool {
-        let myComps: DateComponents = Calendar.current.dateComponents(comps, from: self)
-        let otherComps: DateComponents = Calendar.current.dateComponents(comps, from: otherDate)
-        return Calendar.current.date(from: myComps) == Calendar.current.date(from: otherComps)
-    }
-    
     public func isSameDayAs(_ otherDate: Date) -> Bool {
-        return isSameXAs(otherDate, comps: [.year, .month, .day])
+        return Calendar.current.compare(self, to: otherDate, toGranularity: .day) == .orderedSame
     }
     
     public static var rangeFromToday: PartialRangeFrom<Date> {
--- ToDueTests/DateExtensionTests.swift
+++ ToDueTests/DateExtensionTests.swift
@@ -25,5 +25,29 @@ final class DateExtensionTests: XCTestCase {
         startOfMonth = date.startOfThisMonth
         XCTAssertEqual(startOfMonth, actualStartOfMonth)
     }
+    
+    func testIsSameDayAs() throws {
+        let calendar = Calendar.current
+        
+        var date = calendar.date(from: DateComponents(year: 2023, month: 7, day: 8, hour: 4, minute: 54))!
+        var otherDate = calendar.date(from: DateComponents(year: 2023, month: 7, day: 8, hour: 1, minute: 24))!
+        XCTAssertTrue(date.isSameDayAs(otherDate))
+        
+        date = calendar.date(from: DateComponents(year: 2023, month: 7, day: 4, hour: 4, minute: 54))!
+        otherDate = calendar.date(from: DateComponents(year: 2023, month: 7, day: 8, hour: 1, minute: 24))!
+        XCTAssertFalse(date.isSameDayAs(otherDate))
+        
+        date = calendar.date(from: DateComponents(year: 2023, month: 7, day: 4))!
+        otherDate = calendar.date(from: DateComponents(year: 2023, month: 7, day: 4, hour: 1, minute: 24))!
+        XCTAssertTrue(date.isSameDayAs(otherDate))
+        
+        date = calendar.date(from: DateComponents(year: 2022, month: 7, day: 4))!
+        otherDate = calendar.date(from: DateComponents(year: 2023, month: 7, day: 4))!
+        XCTAssertFalse(date.isSameDayAs(otherDate))
+        
+        date = calendar.date(from: DateComponents(year: 2023, month: 6, day: 4))!
+        otherDate = calendar.date(from: DateComponents(year: 2023, month: 7, day: 4))!
+        XCTAssertFalse(date.isSameDayAs(otherDate))
+    }
 
 }
